7c1bec4020af9cdac4ac7263d46a526a81cc051b
CryptoRAT <lmelliot@gmail.com>
2023-11-12 17:24:28 -0500

    think I have test working

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App.js';
+import App from './App';
 
 test('renders select random survivor button', () => {
   render(<App />);
-  const buttonElement = screen.getByText(/Select Random Survivor/i);
+  const buttonElement = screen.getByText(/Generate Random Survivor Build/i);
   expect(buttonElement).toBeInTheDocument();
 });
