c13a4f75d171cc6fc57c81f6cf1f7bf4acd6cfa5
Luke Elliot <lmelliot@gmail.com>
2023-11-28 08:17:23 -0500

    updated to match the changes in the backend around the urls and some other things.

--- src/App.js
+++ src/App.js
@@ -1,140 +1,26 @@
 import React, {Component} from "react";
-import axios from "axios";
 import "./App.css";
-import RandomSurvivor from "./RandomSurvivor";
-
-const defaultRandomSurvivor =
-    {
-        id: 1,
-        name: "Ace Visconti",
-        image_path: "static/survivors/ace_visconti.jpg",
-    };
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Login from "./Login";
+import GenerateRandomSurvivor from "./GenerateRandomSurvivor";
 
 
-const defaultSurvivorsRandomPerkList = [
-    {
-        "id": 1,
-        "name": "Ace in the Hole",
-        "type": "Survivor",
-        "owner": "Ace Visconti",
-        "image_path": "static/perks/ace_in_the_hole.webp"
-    },
-    {
-        "id": 2,
-        "name": "Open-Handed",
-        "type": "Survivor",
-        "owner": "Ace Visconti",
-        "image_path": "static/perks/open_handed.webp"
-    },
-    {
-        "id": 3,
-        "name": "Up the Ante",
-        "type": "Survivor",
-        "owner": "Ace Visconti",
-        "image_path": "static/perks/up_the_ante.webp"
-    },
-    {
-        "id": 4,
-        "name": "Bond",
-        "type": "Survivor",
-        "owner": "Dwight Fairfield",
-        "image_path": "static/perks/bond.webp"
-    },
-]
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            randomSurvivor: defaultRandomSurvivor,
-            randomSurvivorPerks: defaultSurvivorsRandomPerkList,
-        };
-    }
-
-    componentDidMount() {
-        console.log("Entering componentDidMount");
-        this.loadInitialRandomData();
-        console.log("Leaving componentDidMount");
-    }
-
-    loadInitialRandomData = () => {
-        console.log("Entering loadInitialRandomData");
-        this.calculateRandom(true);
-        console.log("Leaving loadInitialRandomData");
-    };
-
-    calculateRandom = (status) => {
-        console.log("Entering calculateRandom");
-        if (status) {
-            this.getRandomSurvivor();
-            this.getRandomSurvivorPerks();
-        }
-        console.log("Leaving calculateRandom");
-        return this.setState({ randomCalculated: status });
-    };
-
-    renderGenerateRandomButton = () => {
-        console.log("Entering renderGenerateRandomButton");
-        return (
-            <div>
-                <button className="btn btn-primary" onClick={() => {
-                    this.calculateRandom(true);
-                }}>
-                    Generate Random Survivor Build
-                </button>
-            </div>
-        );
-    };
 
     render() {
         return (
-            <main className="container">
-                <h1 className="text-white text-uppercase text-center my-4">Dead by Daylight Build Randomizer App</h1>
-                <div className="row">
-                    <div className="col-md-6 col-sm-10 mx-auto p-0">
-                        <div className="card p-3">
-                            <div className="mb-4">
-                                {this.renderGenerateRandomButton()}
-                            </div>
-                            <RandomSurvivor
-                                survivor={this.state.randomSurvivor}
-                                perks={this.state.randomSurvivorPerks}
-                            />
-                        </div>
-                    </div>
-                </div>
-            </main>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/login" element={<Login/>} />
+                    <Route path="/" element={<GenerateRandomSurvivor/>} />
+                </Routes>
+            </BrowserRouter>
+
         );
     }
 
 
-    getRandomSurvivor() {
-        console.log("Entering getRandomSurvivor");
-        // Get a random survivor
-        axios
-            .get(process.env.REACT_APP_DBD_RANDOMIZER_SERVICE_URL + "api/survivor/random")
-            .then((res) => {
-                const survivorData = Array.isArray(res.data) ? res.data[0] : res.data;
-                this.setState((prevState) => {
-                    return { randomSurvivor: survivorData };
-                }, () => {
-                    console.log("Updated State with Survivor Data:", this.state.randomSurvivor);
-                    console.log("Leaving getRandomSurvivor");
-                });
-            })
 
-            .catch((err) => console.log(err));
-    }
-
-    getRandomSurvivorPerks() {
-        console.log("Entering getRandomSurvivorPerks");
-        // Get random survivor perks
-        axios.get(process.env.REACT_APP_DBD_RANDOMIZER_SERVICE_URL + "api/survivor/perks/random")
-            .then((res) => this.setState({ randomSurvivorPerks: res.data }, () => {
-                console.log("LUKE:" + JSON.stringify(this.state.randomSurvivorPerks));
-                console.log("Leaving getRandomSurvivorPerks");
-            }))
-            .catch((err) => console.log(err));
-    }
 }
 
 export default App;
---
+++ src/GenerateRandomSurvivor.js
@@ -0,0 +1,139 @@
+import RandomSurvivor from "./RandomSurvivor";
+import React, { useState } from "react";
+import axios from "axios";
+
+const GenerateRandomSurvivor = () => {
+    const [randomSurvivor, setRandomSurvivor] = useState({
+        id: 1,
+        name: "Ace Visconti",
+        image_path: "static/survivors/ace_visconti.jpg",
+    });
+    const [randomSurvivorPerks, setRandomSurvivorPerks] = useState([
+        {
+            "id": 1,
+            "name": "Ace in the Hole",
+            "type": "Survivor",
+            "owner": "Ace Visconti",
+            "image_path": "static/perks/ace_in_the_hole.webp"
+        },
+        {
+            "id": 2,
+            "name": "Open-Handed",
+            "type": "Survivor",
+            "owner": "Ace Visconti",
+            "image_path": "static/perks/open_handed.webp"
+        },
+        {
+            "id": 3,
+            "name": "Up the Ante",
+            "type": "Survivor",
+            "owner": "Ace Visconti",
+            "image_path": "static/perks/up_the_ante.webp"
+        },
+        {
+            "id": 4,
+            "name": "Bond",
+            "type": "Survivor",
+            "owner": "Dwight Fairfield",
+            "image_path": "static/perks/bond.webp"
+        },
+    ]);
+
+    const renderGenerateRandomButton = () => {
+        console.log("Entering renderGenerateRandomButton");
+        return (
+            <div>
+                <button className="btn btn-primary" onClick={() => calculateRandom(true)}>
+                    Generate Random Survivor Build
+                </button>
+            </div>
+        );
+    };
+
+    const calculateRandom = (status) => {
+        console.log("Entering calculateRandom");
+        if (status) {
+            getRandomSurvivor();
+            getRandomSurvivorPerks();
+        }
+        console.log("Leaving calculateRandom");
+    };
+
+    const getRandomSurvivor = () => {
+        console.log("Entering getRandomSurvivor");
+        // Get a random survivor
+        console.log("service url: " + process.env.REACT_APP_DBD_RANDOMIZER_SERVICE_URL);
+        axios
+            .post(
+                process.env.REACT_APP_DBD_RANDOMIZER_SERVICE_URL + "api/survivor/random/",
+                // Data to be sent in the request body, if any (optional)
+                { action: 'random' },
+                // Config object with additional options
+                {
+                    headers: {
+                        'Content-Type': 'application/json', // Make sure to set the appropriate content type
+                    },
+                    params: {
+                        // Custom parameters if needed
+                    },
+                }
+            )
+            .then((res) => {
+                console.log("Inside then block");
+                const survivorData = Array.isArray(res.data) ? res.data[0] : res.data;
+                setRandomSurvivor(survivorData);
+                console.log("Updated State with Survivor Data:", survivorData);
+                console.log("Leaving getRandomSurvivor");
+            })
+            .catch((err) => console.log(err));
+    };
+
+    const getRandomSurvivorPerks = () => {
+        console.log("Entering getRandomSurvivorPerks");
+        // Get random survivor perks
+        axios
+            .get(process.env.REACT_APP_DBD_RANDOMIZER_SERVICE_URL + "api/perk/survivor/random/")
+            .then((res) => {
+                console.log("Inside then block");
+                setRandomSurvivorPerks(res.data);
+                console.log("LUKE:" + JSON.stringify(res.data));
+                console.log("Leaving getRandomSurvivorPerks");
+            })
+            .catch((err) => console.log(err));
+    }
+
+    // const loadInitialRandomData = () => {
+    //     console.log("Entering loadInitialRandomData");
+    //     calculateRandom(true);
+    //     console.log("Leaving loadInitialRandomData");
+    // };
+    //
+    // useEffect(() => {
+    //     console.log("Entering useEffect");
+    //     loadInitialRandomData();
+    //     console.log("Leaving useEffect");
+    // }, []); // Empty dependency array means this will run once on mount
+    //
+
+
+    return (
+        <main className="container">
+            <h1 className="text-white text-uppercase text-center my-4">
+                Dead by Daylight Build Randomizer App
+            </h1>
+            <div className="row">
+                <div className="col-md-6 col-sm-10 mx-auto p-0">
+                    <div className="card p-3">
+                        <div className="mb-4">{renderGenerateRandomButton()}</div>
+                        <RandomSurvivor
+                            survivor={randomSurvivor}
+                            perks={randomSurvivorPerks}
+                        />
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default GenerateRandomSurvivor;
