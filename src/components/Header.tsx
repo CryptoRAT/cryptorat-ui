a13525dec5ede742841f18c9fc96fba2dffa45bd
Luke Elliot <luke.elliot@va.gov>
2024-01-25 08:58:43 -0500

    Updated the default perks to my favorite build.

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import '../css/Header.css';
+
+let Header: React.FC = () => {
+
+    return (
+        <header>
+            <div className="header-banner">
+                <h1 className="header-h1">Lupo19's Neighborhood</h1>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
